f2e80a236c7d3f7667e70133ad0f4c795b0a436b
Preeti Toppo <toppopreeti191@gmail.com>
2026-04-09 17:03:42 +0530

    chore: update Dockerfile and app

--- server/app.py
+++ server/app.py
@@ -108,5 +108,10 @@ def list_tasks():
     }
 
 
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    main()
